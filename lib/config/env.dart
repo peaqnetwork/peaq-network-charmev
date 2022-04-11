91c682e7bfef3a29d7705fe3aa36819aaf715821
Iredia Ebikade <40021650+irediaes@users.noreply.github.com>
2022-04-11 21:14:38 +0200

    fix: updated node url to agung

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -75,10 +75,8 @@ class Env {
   static const String extrinsicSuccessEvent = "ExtrinsicSuccess";
   static const String extrinsicFailedEvent = "ExtrinsicFailed";
   static const String approved = "Approved";
-  // static const String peaqTestnet = "wss://wss.test.peaq.network";
-  static const String peaqTestnet = "ws://10.0.2.2:9944";
-  // static const String scaleCodecBaseURL = "https://codec.test.peaq.network";
-  static const String scaleCodecBaseURL = "http://10.0.2.2:3030";
+  static const String peaqTestnet = "wss://wss.agung.peaq.network";
+  // static const String peaqTestnet = "ws://10.0.2.2:9944";
   static const String didDocAttributeName = "v2";
   static const String eventURL = "/events";
   static const String multisigURL = "/multisig";
