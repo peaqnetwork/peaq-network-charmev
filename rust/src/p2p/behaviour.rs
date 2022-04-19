559872103b22843a03fe1ae124fa5c7d0d27d41e
Iredia Ebikade <40021650+irediaes@users.noreply.github.com>
2022-04-19 21:41:21 +0200

    fix: updated swarm event behaviour to create new instance on new connection request

--- rust/src/p2p/behaviour.rs
+++ rust/src/p2p/behaviour.rs
@@ -38,7 +38,11 @@ pub(crate) static mut EVENTS: Lazy<Mutex<VecDeque<Vec<u8>>>> =
 
 // Static GLOBAL variable of SWARM so other event publishing function can use it
 // outside of this scope
-pub(crate) static mut EVENT_BEHAVIOUR: Lazy<Mutex<Swarm<EventBehaviour>>> = Lazy::new(|| {
+pub(crate) static mut EVENT_BEHAVIOUR: Lazy<Mutex<Option<Swarm<EventBehaviour>>>> =
+    Lazy::new(|| Mutex::new(None));
+
+// use to create new instance of swarm every time a new connection is requested
+fn init_swarm() -> Result<Swarm<EventBehaviour>, Box<dyn Error>> {
     let gossipsub_config = GossipsubConfigBuilder::default()
         .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
         .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
@@ -100,54 +104,60 @@ pub(crate) static mut EVENT_BEHAVIOUR: Lazy<Mutex<Swarm<EventBehaviour>>> = Lazy
             }))
             .build()
     };
-    Mutex::new(swarm)
-});
+
+    Ok(swarm)
+}
 
 #[tokio::main]
 pub async fn connect(peer_url: String) -> Result<(), Box<dyn Error>> {
+    let new_swarm = init_swarm().unwrap();
     let swarm;
     unsafe {
+        // set a new instance of swarm event behaviour
+        *EVENT_BEHAVIOUR.lock().unwrap() = Some(new_swarm);
         swarm = EVENT_BEHAVIOUR.get_mut().unwrap();
     }
-    // Listen on all interfaces and whatever port the OS assigns
-    swarm
-        .listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
-        .unwrap();
+    if let Some(swm) = swarm {
+        // Listen on all interfaces and whatever port the OS assigns
+        swm.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
+            .unwrap();
 
-    // Dial another peer address if supplied
-    if let Some(to_dial) = Some(peer_url) {
-        let address: Multiaddr = to_dial.parse().expect("User to provide valid address.");
-        match swarm.dial(address.clone()) {
-            Ok(_) => {
-                event::add_event_to_global(msg::EventType::PEER_CONNECTED);
-                trace!("Dialed {:?}", address);
-            }
-            Err(e) => {
-                event::add_event_to_global(msg::EventType::PEER_CONNECTION_FAILED);
-
-                trace!("Dial {:?} failed: {:?}", address, e);
-            }
-        };
-    }
-
-    // Read full lines from stdin
-    loop {
-        tokio::select! {
-
-                // listening to swarm events
-                event = swarm.select_next_some() => match event {
-                    SwarmEvent::NewListenAddr { address, .. } => {
-                        trace!("Listening on {:?}", address);
-                    },
-                    SwarmEvent::ConnectionClosed {..} => {
-                        trace!("P2P Connection closed!");
-                        break;
-                    }
-                    _ => {}
+        // Dial another peer address if supplied
+        if let Some(to_dial) = Some(peer_url) {
+            let address: Multiaddr = to_dial.parse().expect("User to provide valid address.");
+            match swm.dial(address.clone()) {
+                Ok(_) => {
+                    event::add_event_to_global(msg::EventType::PEER_CONNECTED);
+                    trace!("Dialed {:?}", address);
                 }
+                Err(e) => {
+                    event::add_event_to_global(msg::EventType::PEER_CONNECTION_FAILED);
 
+                    trace!("Dial {:?} failed: {:?}", address, e);
+                }
+            };
+        }
+
+        // Read full lines from stdin
+        loop {
+            tokio::select! {
+
+                    // listening to swarm events
+                    event = swm.select_next_some() => match event {
+                        SwarmEvent::NewListenAddr { address, .. } => {
+                            trace!("Listening on {:?}", address);
+                        },
+                        SwarmEvent::ConnectionClosed {..} => {
+                            trace!("P2P Connection closed!");
+                            break;
+                        }
+                        _ => {}
+                    }
+
+            }
         }
     }
+
     Ok(())
 }
 
@@ -161,12 +171,14 @@ pub fn disconnect(peer_id: String) -> Result<(), Box<dyn Error>> {
         swarm = EVENT_BEHAVIOUR.get_mut().unwrap();
         topic = EVENT_TOPIC.get_mut().unwrap().clone();
     }
+    if let Some(swm) = swarm {
+        if let Some(top) = topic {
+            swm.behaviour_mut().gossip.unsubscribe(&top).unwrap();
+        }
 
-    if let Some(top) = topic {
-        swarm.behaviour_mut().gossip.unsubscribe(&top).unwrap();
+        swm.disconnect_peer_id(peer_id).unwrap();
     }
 
-    swarm.disconnect_peer_id(peer_id).unwrap();
     Ok(())
 }
 
